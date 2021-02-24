567b2eadc94d45d95e1a97c2198e1f100d68f349
Jonathan F. K <jonathanfk1@gmail.com>
2021-02-24 10:42:44 -0300

    Criando alocador, alterando base path.

---
+++ src/pages/Alocar/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState, FormEvent } from 'react';
+
+import PageHeader from '../PageHeader';
+
+import './styles.css'
+import Input from '../../components/Input';
+import api from '../../services/api';
+import ContainerItem, { Container } from '../ContainerItem';
+
+function Alocar() {
+    const [containers, setContainers] = useState([]);
+
+    // const [id, setId] = useState('');
+
+    async function searchContainers(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('containers', {
+            params: {
+                // id
+            }
+        });
+
+        setContainers(response.data);
+    }
+
+    return (
+        <div id="page-container-list" className="container">
+            <PageHeader title="Estes são os containers disponíveis.">
+                <form id="search-containers" onSubmit={searchContainers}>
+                    {/* <Input 
+                        type="id" 
+                        name="id"
+                        label="ID"
+                        value={id}
+                        onChange={ e => { setId(e.target.value) } }
+                    /> */}
+                    <button type="submit">
+                        Buscar
+                    </button>
+                </form>
+            </PageHeader>
+
+            <main>
+                {containers.map((container: Container) => {
+                    return <ContainerItem key={container._id} container={container} />
+                })}
+            </main>
+        </div>
+    )
+}
+
+export default Alocar;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing';
+import Alocar from './pages/Alocar';
 import ContainerForm from './pages/ContainerForm';
 import ContainerList from './pages/ContainerList';
 import GrupoCaixasForm from './pages/GrupoCaixasForm';
@@ -10,8 +11,9 @@ import GrupoCaixasList from './pages/GrupoCaixasList';
 
 function Routes() {
     return (
-        <BrowserRouter basename="/alocacao-front">
+        <BrowserRouter basename="/alocador-front">
             <Route path="/" exact component={Landing} />
+            <Route path="/alocar" component={Alocar} />
             <Route path="/container/criar" component={ContainerForm} />
             <Route path="/container/listar" component={ContainerList} />
             <Route path="/grupocaixas/criar" component={GrupoCaixasForm} />
